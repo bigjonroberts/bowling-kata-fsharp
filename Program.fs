10652d58510f083804cc9c0dec00595c4444752e
Jon Roberts <jroberts@findmymobilehome.com>
2022-11-24 16:52:33 -0600

    replace Bonus with TenthFrame

--- Program.fs
+++ Program.fs
@@ -4,8 +4,7 @@ type Frame =
   | Strike
   | Spare of int
   | Partial of int * int
-  | Bonus of int
-  //| TenthFrame of int * ((int * int option) option)
+  | TenthFrame of int * int * int option
 
 module Frame =
   let private toInt =
@@ -21,37 +20,45 @@ module Frame =
       | c :: '/' :: [] -> c |> toInt |> Spare
       | x :: y :: [] -> Partial (toInt x, toInt y)
       | xs -> failwithf "Invalid Frame: %A" xs
-  let parseBonus (xs: char seq) =
+  let parseTenth (xs: char seq) =
     xs
     |> List.ofSeq
     |> function
-        | 'X' :: [] -> [ Bonus 10 ]
-        | c :: '/' :: [] ->
-          let x = toInt c
-          [ Bonus x; Bonus (10 - x) ]
-        | x :: y :: [] -> [ Bonus (toInt x); Bonus (toInt y) ]
-        | c :: [] -> [ c |> toInt |> Bonus ]
-        | xs -> failwithf "Invalid Bonus Frame: %A" xs
+        | 'X' :: c2  :: '/' :: [] -> c2 |> toInt |> fun x2 -> TenthFrame (10,x2, Some (10 - x2))
+        | 'X' :: c1  :: c2  :: [] -> TenthFrame (10, toInt c1, Some(toInt c2))
+        | c1  :: '/' :: c2  :: [] -> c1 |> toInt |> fun x -> TenthFrame (x, 10 - x, c2 |> toInt |> Some)
+        | c1  :: c2  :: []        -> TenthFrame (toInt c1, toInt c2, None)
+        | _ -> failwithf "Invalid Tenth Frame: %A" xs
+
+  let calcTenth (x1, x2, bonus) = (bonus |> Option.defaultValue 0) + x1 + x2
 
   let private rand = System.Random()
 
   let random () =
-    match rand.Next(10) with
+    match rand.Next 11 with
     | 10 -> Strike
     | x ->
-      match (x, rand.Next(10 - x)) with
+      match (x, rand.Next(11 - x)) with
       | (x,y) when x+y=10 -> Spare x
       | (x,y) -> Partial (x,y)
 
-  type BonusFrameCount = | Zero | One | Two
-
-  let randomBonus (numFrames: BonusFrameCount) =
-    match numFrames with
-    | Zero -> []
-    | One -> [ 10 |> rand.Next |> Bonus ]
-    | Two ->
-      let x = rand.Next(10)
-      [ Bonus x; Bonus (rand.Next(10 - x)) ]
+  let randomTenth () : Frame =
+    ( match rand.Next 11 with
+      | 10 ->
+        let y = rand.Next 11
+        let z =
+          if y = 10 then
+            11 |> rand.Next
+          else
+            (11 - y) |> rand.Next
+        (10,y,Some z)
+      | x ->
+        let y = rand.Next(11 - x)
+        if x + y = 10 then
+          (x,y, 11 |> rand.Next |> Some)
+        else
+          (x,y,None) )
+    |> TenthFrame
 
 type Game = Frame list    
 
@@ -59,50 +66,48 @@ module Game =
   let parse (s: string) : Game =
     s.Split(' ')
     |> Array.mapi (fun i s' ->
-      if i < 10 then
-        [ Frame.parse s' ]
+      if i < 9 then
+        Frame.parse s'
       else
-        Frame.parseBonus s')
+        Frame.parseTenth s')
     |> List.ofArray
-    |> List.collect id
 
   let private (|StrikeThrown|_|) (game: Game) =
     match game with
     | Strike :: tail ->
-      let addScore (score: int) =
+      let frameScore =
         match tail with
-        | Bonus x :: Bonus y :: [] -> score + 10 + x + y
-        | Spare _ :: _ -> score + 20
-        | Strike :: Strike :: _ -> score + 30
-        | Strike :: Spare x :: _
-        | Strike :: Partial (x,_) :: _
-        | Strike :: Bonus x :: _ -> score + 20 + x
-        | Partial (x,y) :: _ -> score + x + y
+        | Spare _            :: _                        -> 20
+        | Strike             :: Strike             :: _  -> 30
+        | Strike             :: Spare x            :: _
+        | Strike             :: Partial (x,_)      :: _ 
+        | Strike             :: TenthFrame (x,_,_) :: _  -> 20 + x
+        | Partial (x,y)      :: _
+        | TenthFrame (x,y,_) :: []                       -> 10 + x + y
         | _ -> failwithf "Invalid remaining frames: %A" game
-      Some (addScore, tail)
+      Some (frameScore, tail)
     | _ -> None
 
   let private (|SpareThrown|_|) (game: Game) =
     match game with
     | Spare _ :: tail ->
-      let addScore (score: int) =
+      let frameScore =
         match tail with
-        | Bonus x :: [] -> score + 10 + x
-        | Strike :: _ -> score + 20
-        | Spare x :: _
-        | Partial (x,_) :: _ -> score + 10 + x
+        | Strike             :: _ -> 20
+        | Spare x            :: _
+        | TenthFrame (x,_,_) :: []
+        | Partial (x,_)      :: _ -> 10 + x
         | _ -> failwithf "Invalid remaining frames: %A" game
-      Some (addScore, tail)
+      Some (frameScore, tail)
     | _ -> None
 
   let rec private calc (game: Game) (score: int) =
     match game with
-    | StrikeThrown (addScore, tail) -> addScore score |> calc tail
-    | SpareThrown (addScore, tail) -> addScore score |> calc tail
+    | StrikeThrown (frameScore, tail) -> score + frameScore |> calc tail
+    | SpareThrown (frameScore, tail) -> score + frameScore |> calc tail
     | Partial (x,y) :: tail -> score + x + y |> calc tail
-    | []
-    // into bonus frames, so no additional scoring
-    | Bonus _ :: _ -> score
+    | TenthFrame (x1,x2,bonus) :: [] -> score + Frame.calcTenth (x1,x2,bonus)
+    | [] -> score
     | _ -> failwithf "Invalid remaining frames: %A" game
 
   let calculate (game: Game) = calc game 0
@@ -111,9 +116,11 @@ module Game =
     game |> List.iter (printfn "%A")
     score |> printfn "-----------------\n\rScore: %i\r\n******************\r\n"
 
-[ "X X X X X X X X X X X X"
+  let random _ : Game = List.init 10 (function | 9 -> Frame.randomTenth () | _ -> Frame.random ())
+    
+[ "X X X X X X X X X XXX"
   "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-"
-  "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5" ]
-|> List.iter (fun s ->
-  let game = Game.parse s
-  (game, Game.calculate game) |> Game.display)
+  "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5" ]
+|> List.map Game.parse
+|> List.append (List.init 30 Game.random)
+|> List.iter (fun game -> (game, Game.calculate game) |> Game.display)
